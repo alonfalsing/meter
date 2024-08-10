c4708aa9201c7f91a60e11ebf164fb29545fa181
tsai phan <phanalpha@hotmail.com>
2024-08-11 01:41:45 +0800

    Add calendar week and month

--- src/DateRange.ts
+++ src/DateRange.ts
@@ -5,18 +5,15 @@ import invariant from "tiny-invariant";
  * Date range is a pair of calendar dates, `from` and `thru` (inclusive).
  */
 export class DateRange {
-  static month(year: number, month: number): DateRange {
-    return new DateRange(
-      new CalendarDate(year, month, 1),
-      new CalendarDate(year, month + 1, 0),
-    );
-  }
-
   constructor(
     readonly from: CalendarDate,
     readonly thru: CalendarDate,
   ) {
     invariant(from <= thru, `Invalid date range: ${from} ~ ${thru}`);
+  }
+
+  contains(date: CalendarDate) {
+    return this.from <= date && date <= this.thru;
   }
 
   get length(): number {
@@ -27,5 +24,28 @@ export class DateRange {
     return Array.from({ length: this.length }, (_, i) =>
       this.from.add(i, "days"),
     );
+  }
+}
+
+export class CalendarWeek extends DateRange {
+  constructor(date: CalendarDate) {
+    const sunday = date.add(-date.dayOfWeek, "days");
+    super(sunday, sunday.add(6, "days"));
+  }
+}
+
+export class CalendarMonth extends DateRange {
+  readonly startOfMonth: CalendarDate;
+  readonly endOfMonth: CalendarDate;
+
+  constructor(year: number, month: number) {
+    const startOfMonth = new CalendarDate(year, month, 1);
+    const endOfMonth = new CalendarDate(year, month + 1, 0);
+    const firstWeek = new CalendarWeek(startOfMonth);
+    const lastWeek = new CalendarWeek(endOfMonth);
+
+    super(firstWeek.from, lastWeek.thru);
+    this.startOfMonth = startOfMonth;
+    this.endOfMonth = endOfMonth;
   }
 }
--- src/__tests__/DateRange.test.ts
+++ src/__tests__/DateRange.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "@jest/globals";
-import { CalendarDate, DateRange } from "..";
+import { CalendarDate, CalendarMonth, CalendarWeek, DateRange } from "..";
 
 describe("DateRange", () => {
   test.each([
@@ -30,6 +30,27 @@ describe("DateRange", () => {
     ).toThrowError("Invalid date range: 2021-01-31 ~ 2021-01-01");
   });
 
+  test.each([
+    {
+      name: "contains a date",
+      from: new CalendarDate(2021, 1, 1),
+      thru: new CalendarDate(2021, 1, 31),
+      date: new CalendarDate(2021, 1, 15),
+      expected: true,
+    },
+    {
+      name: "does not contain a date",
+      from: new CalendarDate(2021, 1, 1),
+      thru: new CalendarDate(2021, 1, 31),
+      date: new CalendarDate(2021, 2, 1),
+      expected: false,
+    },
+  ])("$name", ({ from, thru, date, expected }) => {
+    const range = new DateRange(from, thru);
+
+    expect(range.contains(date)).toBe(expected);
+  });
+
   test("computes the length", () => {
     const range = new DateRange(
       new CalendarDate(2021, 1, 1),
@@ -55,11 +76,24 @@ describe("DateRange", () => {
       new CalendarDate(2021, 1, 9),
     ]);
   });
+});
 
-  test("builds a month range", () => {
-    const range = DateRange.month(2021, 1);
+describe("CalendarWeek", () => {
+  test("constructs a CalendarWeek", () => {
+    const week = new CalendarWeek(new CalendarDate(2021, 1, 1));
 
-    expect(range.from).toEqual(new CalendarDate(2021, 1, 1));
-    expect(range.thru).toEqual(new CalendarDate(2021, 1, 31));
+    expect(week.from).toEqual(new CalendarDate(2020, 12, 27));
+    expect(week.thru).toEqual(new CalendarDate(2021, 1, 2));
+  });
+});
+
+describe("CalendarMonth", () => {
+  test("constructs a CalendarMonth", () => {
+    const month = new CalendarMonth(2021, 1);
+
+    expect(month.from).toEqual(new CalendarDate(2020, 12, 27));
+    expect(month.thru).toEqual(new CalendarDate(2021, 2, 6));
+    expect(month.startOfMonth).toEqual(new CalendarDate(2021, 1, 1));
+    expect(month.endOfMonth).toEqual(new CalendarDate(2021, 1, 31));
   });
 });
